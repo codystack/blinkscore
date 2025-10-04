3bb6fa137f3afdf6ea0dddde2224f30f95ed1459
codystack <ebuzzadvert@gmail.com>
2025-10-04 22:30:01 +0100

    Footer added

---
+++ components/footer.php
@@ -0,0 +1,110 @@
+            <!-- start: Footer Section -->
+            <footer class="tj-footer-section footer-2 section-gap-x pt-0">
+                <div class="footer-main-area">
+                    <div class="container">
+                        <div class="row justify-content-between">
+                            <div class="col-xl-3 col-md-6">
+                                <div class="footer-widget footer-col-1 wow fadeInUp" data-wow-delay=".1s">
+                                    <div class="footer-logo">
+                                        <a href="./">
+                                            <img src="./assets/images/logo_light.svg" alt="Logos">
+                                        </a>
+                                    </div>
+                                    <div class="footer-text">
+                                        <p>BlinksCore is a trusted financial company based in Lagos, Nigeria, specializing in providing Proof of Funds and Employment Salary History services.</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-xl-3 col-md-6">
+                                <div class="footer-widget footer-col-2 widget-nav-menu wow fadeInUp" data-wow-delay=".3s">
+                                    <h5 class="title">Company</h5>
+                                    <ul>
+                                        <li><a href="#">Home</a></li>
+                                        <li><a href="#">About us</a></li>
+                                        <li><a href="contact">Contact us</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="col-xl-3 col-md-6">
+                                <div class="footer-widget footer-col-3 widget-nav-menu wow fadeInUp" data-wow-delay=".5s">
+                                    <h5 class="title">Quick Links</h5>
+                                    <ul>
+                                        <li><a href="faq">FAQ's</a></li>
+                                        <li><a href="terms">Terms of use</a></li>
+                                        <li><a href="privacy">Privacy policy</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        <div class="col-xl-3 col-md-6">
+                            <div class="footer-widget widget-contact wow fadeInUp" data-wow-delay=".7s">
+                            <h5 class="title">Our Office</h5>
+                            <div class="footer-contact-info">
+                                <div class="contact-item">
+                                    <span>Shop E 284, Road 2, Ikota Shopping Complex, Ikota Lagos.</span>
+                                </div>
+                                <!-- <div class="contact-item">
+                                    <a href="tel:10095447818">P: +1 (009) 544-7818</a>
+                                    <a href="mailto:support@bexon.com">M: support@bexon.com</a>
+                                </div> -->
+                                <div class="social-links style-3">
+                                    <ul>
+                                        <li><a href="https://www.facebook.com/" target="_blank"><i
+                                                class="fa-brands fa-facebook-f"></i></a>
+                                        </li>
+                                        <li><a href="https://www.instagram.com/" target="_blank"><i
+                                                class="fa-brands fa-instagram"></i></a>
+                                        </li>
+                                        <li><a href="https://x.com/" target="_blank"><i class="fa-brands fa-x-twitter"></i></a></li>
+                                        <li><a href="https://www.linkedin.com/" target="_blank"><i
+                                                class="fa-brands fa-linkedin-in"></i></a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="tj-copyright-area-2">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-12">
+                                <div class="copyright-content-area">
+                                    <div class="copyright-text">
+                                        <p>© <script>document.write(new Date().getFullYear());</script> BlinksCore. All right reserved. Built by <a href="https://webify.com.ng" target="_blank">Webify</a></p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="bg-shape-1">
+                    <img src="assets/images/shape/pattern-2.svg" alt="">
+                </div>
+                <div class="bg-shape-2">
+                    <img src="assets/images/shape/pattern-3.svg" alt="">
+                </div>
+            </footer>
+            <!-- end: Footer Section -->
+        </div>
+    </div>
+  
+  <script src="assets/js/jquery.min.js"></script>
+  <script src="assets/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/js/gsap.min.js"></script>
+  <script src="assets/js/ScrollSmoother.js"></script>
+  <script src="assets/js/gsap-scroll-to-plugin.min.js"></script>
+  <script src="assets/js/gsap-scroll-trigger.min.js"></script>
+  <script src="assets/js/gsap-split-text.min.js"></script>
+  <script src="assets/js/jquery.nice-select.min.js"></script>
+  <script src="assets/js/swiper.min.js"></script>
+  <script src="assets/js/odometer.min.js"></script>
+  <script src="assets/js/venobox.min.js"></script>
+  <script src="assets/js/appear.min.js"></script>
+  <script src="assets/js/wow.min.js"></script>
+  <script src="assets/js/meanmenu.js"></script>
+  <script src="assets/js/main.js"></script>
+</body>
+
+</html>
--- components/mobile-navbar.php
+++ components/mobile-navbar.php
@@ -4,21 +4,12 @@
             <div class="hamburger_inner">
                 <div class="hamburger_top d-flex align-items-center justify-content-between">
                     <div class="hamburger_logo">
-                        <a href="index.html" class="mobile_logo">
-                        <img src="assets/images/logos/logo-2.webp" alt="Logo">
+                        <a href="./" class="mobile_logo">
+                        <img src="./assets/images/logo_light.svg" width="200" alt="Logo">
                         </a>
                     </div>
                     <div class="hamburger_close">
                         <button class="hamburger_close_btn"><i class="fa-thin fa-times"></i></button>
-                    </div>
-                </div>
-                <div class="hamburger-search-area">
-                    <h5 class="hamburger-title">Search Now!</h5>
-                    <div class="hamburger_search">
-                        <form method="get" action="index.html">
-                        <button type="submit"><i class="tji-search"></i></button>
-                        <input type="search" autocomplete="off" name="s" value="" placeholder="Search here...">
-                        </form>
                     </div>
                 </div>
                 <div class="hamburger_menu">
@@ -29,15 +20,15 @@
                     <div class="contact-info">
                         <div class="contact-item">
                             <span class="subtitle">Phone</span>
-                            <a class="contact-link" href="tel:8089091313">808-909-1313</a>
+                            <a class="contact-link" href="tel:8089091313">+234 913-757-1407</a>
                         </div>
                         <div class="contact-item">
                             <span class="subtitle">Email</span>
-                            <a class="contact-link" href="mailto:info@bexon.com">info@bexon.com</a>
+                            <a class="contact-link" href="mailto:hello@blinkscore.ng">hello@blinkscore.ng</a>
                         </div>
                         <div class="contact-item">
                             <span class="subtitle">Location</span>
-                            <span class="contact-link">993 Renner Burg, West Rond, MT 94251-030</span>
+                            <span class="contact-link">Shop E 284, Road 2, Ikota Shopping Complex, Ikota Lagos.</span>
                         </div>
                     </div>
                 </div>
@@ -46,12 +37,14 @@
                 <h5 class="hamburger-title">Follow Us</h5>
                 <div class="social-links style-3">
                     <ul>
-                        <li><a href="https://www.facebook.com/" target="_blank"><i class="fa-brands fa-facebook-f"></i></a>
+                        <li>
+                            <a href="https://www.facebook.com/blinkscorehq" target="_blank"><i class="fa-brands fa-facebook-f"></i></a>
                         </li>
-                        <li><a href="https://www.instagram.com/" target="_blank"><i class="fa-brands fa-instagram"></i></a>
+                        <li>
+                            <a href="https://www.instagram.com/blinkscorehq" target="_blank"><i class="fa-brands fa-instagram"></i></a>
                         </li>
-                        <li><a href="https://x.com/" target="_blank"><i class="fa-brands fa-x-twitter"></i></a></li>
-                        <li><a href="https://www.linkedin.com/" target="_blank"><i class="fa-brands fa-linkedin-in"></i></a>
+                        <li>
+                            <a href="https://x.com/blinkscorehq" target="_blank"><i class="fa-brands fa-x-twitter"></i></a>
                         </li>
                     </ul>
                 </div>
