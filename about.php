5f637186df68f727c0ad4bc7a786f0ec3685fdc5
codystack <ebuzzadvert@gmail.com>
2025-10-05 09:10:10 +0100

    Contact page added

---
+++ about.php
@@ -0,0 +1,221 @@
+<?php
+include "./components/header.php";
+include "./components/mobile-navbar.php";
+include "./components/navbar-light.php";
+include "./components/sticky-navbar.php";
+?>
+
+    <div id="smooth-wrapper">
+        <div id="smooth-content">
+            <main id="primary" class="site-main">
+                <div class="space-for-header"></div>
+                <section class="tj-page-header section-gap-x" data-bg-image="./assets/images/about.jpg">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <div class="tj-page-header-content text-center">
+                                    <h1 class="tj-page-title">About Us</h1>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="page-header-overlay" data-bg-image="assets/images/shape/pheader-overlay.webp"></div>
+                </section>
+
+                <section id="choose" class="tj-choose-section section-gap">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-12">
+                                <div class="sec-heading-wrap">
+                                    <div class="heading-wrap-content">
+                                        <div class="sec-heading">
+                                            <h2 class="sec-title title-anim"><span>BlinksCore:</span> Your reliable financial verification partner.</h2>
+                                        </div>
+                                        <div class="btn-wrap wow fadeInUp" data-wow-delay=".6s">
+                                            <a class="tj-primary-btn" href="contact">
+                                                <span class="btn-text"><span>Request a Call</span></span>
+                                                <span class="btn-icon"><i class="tji-arrow-right-long"></i></span>
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row row-gap-4 rightSwipeWrap">
+                            <div class="col-lg-12">
+                                <div class="choose-box right-swipe">
+                                    <div class="choose-content">
+                                        <p class="mb-3">BlinksCore is a trusted financial services company headquartered in Lagos, Nigeria. We specialize in providing reliable Proof of Funds (POF) and Employment Salary History documentation to individuals and businesses across the region.</p>
+                                        <p class="mb-3">Our mission is to deliver accurate and verifiable financial documentation that helps our clients meet their financial and compliance requirements with confidence. Whether you’re looking to verify funds for a transaction or establish your employment salary history for official purposes, BlinksCore offers tailored solutions designed to support your needs.</p>
+                                        <p class="mb-3">At BlinksCore, we prioritize integrity, transparency, and customer satisfaction, partnering with reputable banks and institutions to ensure the authenticity of our services. Our experienced team is committed to providing seamless, efficient, and secure documentation services that empower you to achieve your financial goals.</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+
+                <section class="tj-about-section-2 section-gap section-gap-x">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-xl-6 col-lg-6 order-lg-1 order-2">
+                                <div class="about-img-area style-2 wow fadeInLeft" data-wow-delay=".3s">
+                                    <div class="about-img overflow-hidden">
+                                        <img data-speed=".8" src="assets/images/about-img.jpg" alt="">
+                                    </div>
+                                    <div class="box-area style-2">
+                                        <div class="progress-box wow fadeInUp" data-wow-delay=".3s">
+                                            <h4 class="title">Success Rate</h4>
+                                            <ul class="tj-progress-list">
+                                                <li>
+                                                    <h6 class="tj-progress-title">Proof of Funds</h6>
+                                                    <div class="tj-progress">
+                                                        <span class="tj-progress-percent">90%</span>
+                                                        <div class="tj-progress-bar" data-percent="90">
+                                                        </div>
+                                                    </div>
+                                                </li>
+                                                <li>
+                                                    <h6 class="tj-progress-title">Employment Salary History</h6>
+                                                    <div class="tj-progress">
+                                                        <span class="tj-progress-percent">85%</span>
+                                                        <div class="tj-progress-bar" data-percent="85">
+                                                        </div>
+                                                    </div>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-xl-6 col-lg-6 order-lg-2 order-1">
+                                <div class="about-content-area">
+                                    <div class="sec-heading">
+                                        <h2 class="sec-title title-anim">Who We Are,<br /><span>What We Strive For</span></h2>
+                                    </div>
+                                    <p class="mb-5">At BlinksCore, our work is guided by a clear sense of purpose and direction. We are committed to delivering dependable financial documentation solutions that support our clients' goals and uphold the highest standards of integrity and professionalism. Our mission and vision reflect the values that drive us and the future we are building towards.</p>
+                                </div>
+                                <div class="about-bottom-area">
+                                    <div class="mission-vision-box wow fadeInLeft" data-wow-delay=".5s">
+                                        <h4 class="title">Our Mission</h4>
+                                        <p class="desc">To provide trusted, accurate, and timely financial documentation services that empower individuals and businesses to meet regulatory, transactional, and compliance requirements with confidence.</p>
+                                    </div>
+                                    <div class="mission-vision-box wow fadeInRight" data-wow-delay=".5s">
+                                        <h4 class="title">Our Vision</h4>
+                                        <p class="desc">To be the leading provider of reliable financial verification solutions in Nigeria and beyond—setting the standard for integrity, professionalism, and customer-centric service in the financial documentation space.</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="bg-shape-1">
+                        <img src="assets/images/shape/pattern-2.svg" alt="">
+                    </div>
+                    <div class="bg-shape-2">
+                        <img src="assets/images/shape/pattern-3.svg" alt="">
+                    </div>
+                </section>
+
+                <section class="tj-testimonial-section-2 section-bottom-gap section-top-gap">
+                    <div class="container">
+                        <div class="row row-gap-3">
+                            <div class="col-lg-6 order-lg-2">
+                                <div class="testimonial-img-area wow fadeInUp" data-wow-delay=".3s">
+                                    <div class="testimonial-img">
+                                        <img src="./assets/images/testimonial.jpg" alt="">
+                                        <div class="sec-heading style-2">
+                                            <h2 class="sec-title title-anim">Hear from Our Customer.</h2>
+                                        </div>
+                                    </div>
+                                    <div class="box-area">
+                                        <div class="rating-box wow fadeInUp" data-wow-delay=".5s">
+                                            <h2 class="title">4.9</h2>
+                                            <div class="rating-area">
+                                                <div class="star-ratings">
+                                                    <div class="fill-ratings" style="width: 100%">
+                                                        <span>★★★★★</span>
+                                                    </div>
+                                                    <div class="empty-ratings">
+                                                        <span>★★★★★</span>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <span class="rating-text">(80+ Clients Reviews)</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-6 order-lg-1">
+                                <div class="testimonial-wrapper wow fadeInUp" data-wow-delay=".5s">
+                                    <div class="swiper testimonial-slider-2">
+                                        <div class="swiper-wrapper">
+                                            <div class="swiper-slide">
+                                                <div class="testimonial-item">
+                                                    <span class="quote-icon"><i class="tji-quote"></i></span>
+                                                    <div class="desc">
+                                                        <p>BlinksCore came through when I needed a Proof of Funds urgently for an international property deal. The process was seamless, professional, and faster than I expected. I highly recommend their services to anyone who values reliability.</p>
+                                                    </div>
+                                                    <div class="testimonial-author">
+                                                        <div class="author-inner">
+                                                            <div class="author-img">
+                                                                <img src="./assets/images/chinedu.png" alt="">
+                                                            </div>
+                                                            <div class="author-header">
+                                                                <h4 class="title">Chinedu</h4>
+                                                                <span class="designation">Lagos, Nigeria</span>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+
+                                            <div class="swiper-slide">
+                                                <div class="testimonial-item">
+                                                    <span class="quote-icon"><i class="tji-quote"></i></span>
+                                                    <div class="desc">
+                                                        <p>I needed Employment Salary History documentation for a visa application, and BlinksCore handled everything smoothly. Their team was responsive, respectful, and transparent throughout. It felt good to work with a company that knows what they’re doing.</p>
+                                                    </div>
+                                                    <div class="testimonial-author">
+                                                        <div class="author-inner">
+                                                            <div class="author-img">
+                                                                <img src="./assets/images/amina.png" alt="">
+                                                            </div>
+                                                            <div class="author-header">
+                                                                <h4 class="title">Amina</h4>
+                                                                <span class="designation">Abuja, Nigeria</span>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+
+                                            <div class="swiper-slide">
+                                                <div class="testimonial-item">
+                                                    <span class="quote-icon"><i class="tji-quote"></i></span>
+                                                    <div class="desc">
+                                                        <p>At first, I was skeptical, but BlinksCore exceeded my expectations. They provided the POF I needed for a business transaction, and the bank they partnered with was reputable. Solid service from start to finish!</p>
+                                                    </div>
+                                                    <div class="testimonial-author">
+                                                        <div class="author-inner">
+                                                            <div class="author-img">
+                                                                <img src="./assets/images/tunde.png" alt="">
+                                                            </div>
+                                                            <div class="author-header">
+                                                                <h4 class="title">Tunde</h4>
+                                                                <span class="designation">Port Harcourt, Nigeria</span>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="swiper-pagination-area"></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </main>
+
+<?php include "./components/footer.php"; ?>
